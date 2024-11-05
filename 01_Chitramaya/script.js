3d3487cb5d52d5cb371ed82d35f6cdbe41d1dfe8
UDIT BALIYAN <130930448+uditbaliyan@users.noreply.github.com>
2024-11-05 23:47:03 +0530

    Update script.js

--- 01_Chitramaya/script.js
+++ 01_Chitramaya/script.js
@@ -39,23 +39,6 @@ function filterMovies(movieList, searchTerm) {
     return movieList.filter(movie => movie.title.toLowerCase().includes(searchTerm));
 }
 
-// Create movie item
-async function createMovieItem(movie, list) {
-    const movieItem = document.createElement('div');
-    movieItem.className = 'movie-item';
-
-    const img = document.createElement('img');
-    img.src = await fetchMovieImage(movie.title);
-    img.alt = movie.title;
-    img.style.width = '100px';
-
-    const info = document.createElement('div');
-    info.className = 'movie-info';
-    info.innerHTML = `${movie.title} (Rating: ${movie.rating})`;
-
-    movieItem.append(img, info);
-    list.appendChild(movieItem);
-}
 
 // Fetch movie image from OMDb API
 async function fetchMovieImage(title) {
